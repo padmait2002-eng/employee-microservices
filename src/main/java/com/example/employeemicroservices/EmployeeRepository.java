d88984a136c128cc33ec8f3b0122ebab1b9132f4
Padma <padma.it2002@gmail.com>
2026-06-19 17:46:44 -0400

    Updated the files

---
+++ src/main/java/com/example/employeemicroservices/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.employeemicroservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
+
